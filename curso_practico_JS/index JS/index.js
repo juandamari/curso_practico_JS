43e0c7fd70e46fa48568c9ab12daaafc072ff6cf
juan david marin <juan_da-mari@hotmail.com>
2022-05-03 11:15:09 -0500

    enlance con repositorio laboral

---
+++ curso_practico_JS/index JS/index.js
@@ -0,0 +1,8 @@
+const ladoCuadrado = 5;
+console.log("los lados del cuadrado miden: " + ladoCuadrado + "cm");
+
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log("el permimetro del cuadrado es: " + perimetroCuadrado + "cm");
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+console.log("el area del cuadrado es: " + areaCuadrado + "cm2");
