190c15b76a106dba73186c6e56811db506d64017
juan david marin <juan_da-mari@hotmail.com>
2022-05-03 14:51:15 -0500

    terminar cuadro de triangulo2

--- curso_practico_JS/index JS/index.js
+++ curso_practico_JS/index JS/index.js
@@ -12,7 +12,7 @@ console.log("el area del cuadrado es: " + areaCuadrado + "cm2");
 // triangulo
 
 const baseTriangulo = 4;
-const ladoTriangulo = 6 + 6;
+const ladoTriangulo1 = 6;
+const ladoTriangulo2 = 6;
 const alturaTriangulo = 5.5;
-console.log("el perimetro del triangulo miden " + (ladoTriangulo + baseTriangulo) + "cm");
-console.log("el area del triangulo es: " + baseTriangulo * alturaTriangulo + "cm2");
+console.log("los lados del triangulo miden: " + ladoTriangulo1 + "cm, " + ladoTriangulo2 + "cm, " + baseTriangulo + "cm.")
