bb28de3b4f5569aaf7d0e09ddec58a1ee84ea5ea
juan david marin <juan_da-mari@hotmail.com>
2022-05-03 11:23:05 -0500

    suma del area triangulo

--- curso_practico_JS/index JS/index.js
+++ curso_practico_JS/index JS/index.js
@@ -1,3 +1,5 @@
+// cuadrado
+
 const ladoCuadrado = 5;
 console.log("los lados del cuadrado miden: " + ladoCuadrado + "cm");
 
@@ -6,3 +8,9 @@ console.log("el permimetro del cuadrado es: " + perimetroCuadrado + "cm");
 
 const areaCuadrado = ladoCuadrado * ladoCuadrado;
 console.log("el area del cuadrado es: " + areaCuadrado + "cm2");
+
+// triangulo
+
+const baseTriangulo = 4;
+const ladoTriangulo = 6 + 6;
+console.log("los lados del triangulo miden " + (ladoTriangulo + baseTriangulo) + "cm")
