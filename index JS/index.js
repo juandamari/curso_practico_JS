b3af1c87970063fb040bbefcedc69f387d586cab
juan_david <juan_da-mari@hotmail.com>
2022-05-04 21:15:09 -0500

    fusion del repo remoto

--- index JS/index.js
+++
@@ -1,8 +0,0 @@
-const ladoCuadrado = 5;
-console.log("los lados del cuadrado miden: " + ladoCuadrado + "cm");
-
-const perimetroCuadrado = ladoCuadrado * 4;
-console.log("el permimetro del cuadrado es: " + perimetroCuadrado + "cm");
-
-const areaCuadrado = ladoCuadrado * ladoCuadrado;
-console.log("el area del cuadrado es: " + areaCuadrado + "cm2");
