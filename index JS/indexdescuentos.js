2afda28c0e05f613fb7b2fea9541726e22b43771
juan david M <juan_da-mari@hotmail.com>
2022-05-12 23:02:00 -0500

    modelando

---
+++ index JS/indexdescuentos.js
@@ -0,0 +1,31 @@
+//const preciooriginal = 120;
+//const descuento = 18;
+
+//onst porcentajepreciocondescuento = 100 - descuento;
+//const preciocondescuento = (preciooriginal * porcentajepreciocondescuento) / 100;
+
+//console.log({
+    //preciooriginal,
+    //descuento,
+    //porcentajepreciocondescuento,
+    //preciocondescuento,
+//});
+
+function calcularpreciocondescuento (precio, descuento) {
+    const porcentajepreciocondescuento = 100 - descuento;
+    const preciocondescuento = (precio * porcentajepreciocondescuento) / 100;
+
+    return preciocondescuento;
+}
+function clickbotonpreciodescuento () {
+    const inputprice = document.getElementById("inputprice");
+    const pricevalue = inputprice.value;
+
+    const inputdiscount = document.getElementById ("inputdiscount");
+    const discountvalue = inputdiscount.value;
+    const preciocondescuento = calcularpreciocondescuento (pricevalue, discountvalue)
+
+    const resultp = document.getElementById("resultp");
+    resultp.innerText = "el precio total con descuento es: $" + preciocondescuento;
+}
+console.log(document.querySelectorAll("p"))
