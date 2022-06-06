83ce0230641491d8882072218b27b80225f0d11b
juan david pos <juan_da-mari@hotmail.com>
2022-06-06 17:57:46 -0500

    organizando la informacion para hacer el analisis de salarios

---
+++ curso_practico_JS/index JS/salarios.js
@@ -0,0 +1,77 @@
+const colombia = [];
+colombia.push({
+    name: "juan",
+    salary: 1000000
+});
+colombia.push({
+    name: "fernando",
+    salary: 2500000
+});
+colombia.push({
+    name: "fabio",
+    salary: 3000000
+});
+colombia.push({
+    name: "david",
+    salary: 1500000
+});
+colombia.push({
+    name: "camilo",
+    salary: 4000000
+});
+colombia.push({
+    name: "camila",
+    salary: 1200000
+});
+colombia.push({
+    name: "sebastian",
+    salary: 1000000
+});
+colombia.push({
+    name: "diana",
+    salary: 4000000
+});
+colombia.push({
+    name: "yuli",
+    salary: 1400000
+});
+colombia.push({
+    name: "luis",
+    salary: 2000000
+});
+colombia.push({
+    name: "sergio",
+    salary: 1600000
+});
+colombia.push({
+    name: "wislon",
+    salary: 1200000
+});
+colombia.push({
+    name: "felipe",
+    salary: 2000000
+});
+colombia.push({
+    name: "ana",
+    salary: 1000000
+});
+colombia.push({
+    name: "kevin",
+    salary: 2000000
+});
+colombia.push({
+    name: "carlos",
+    salary: 1500000
+});
+colombia.push({
+    name: "fabian",
+    salary: 1800000
+});
+colombia.push({
+    name: "robin",
+    salary: 1000000
+});
+colombia.push({
+    name: "jhon",
+    salary: 1400000
+});
